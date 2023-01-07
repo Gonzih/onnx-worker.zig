23658bab56e01709fa83d6c4dfd2511226e9ba11
Max Gonzih <gonzih@gmail.com>
2023-01-06 18:14:09 -0800

    Clean

--- build.zig
+++ build.zig
@@ -6,14 +6,7 @@ inline fn getThisDir() []const u8 {
 }
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("onnx-worker", "src/main.zig");
